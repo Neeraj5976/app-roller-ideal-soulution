0df525b8e65a7bf6cbb1171d10240bab22e83f82
NEERAJ KUMAR <48087419+Neeraj5976@users.noreply.github.com>
2025-05-07 12:51:19 +0000

    aa

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["g6jts8-5173.csb.app"],
+    allowedHosts:true
   },
 });
